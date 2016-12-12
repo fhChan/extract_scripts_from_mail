f00979c44a0f174b66c9be806517e7b496ac44af
Feihao Chen <feihao_chen@trendmicro.com.cn>
2016-12-12 15:37:38 +0800

    Update dir_csv.py

--- trendx_tool/dir_csv.py
+++ trendx_tool/dir_csv.py
@@ -11,16 +11,7 @@ Usage:
     python tool.py input_dir
     """
 
-if __name__=='__main__':
-    if len(sys.argv)!=2:
-        print_usage()
-        exit(-1)
-
-    folder_path=sys.argv[1]
-    if not os.path.exists(folder_path):
-        print 'folder not exists'
-        exit(0)
-
+def folder_process(folder_path):
     csvfile=file('trendx_decision.csv','wb')
     writer=csv.writer(csvfile)
     writer.writerow(['filename','predict_value','label'])
@@ -31,3 +22,15 @@ if __name__=='__main__':
         p_value=float(m.predictfile(filepath)[0])
         writer.writerow([f,str(p_value),str(1 if p_value>0.5 else 0)])
 
+
+if __name__=='__main__':
+    if len(sys.argv)!=2:
+        print_usage()
+        exit(-1)
+
+    folder_path=sys.argv[1]
+    if not os.path.exists(folder_path):
+        print 'folder not exists'
+        exit(0)
+
+    folder_process(folder_path)
