5c77f625e4689547fcf280da5a699d9862714af1
Feihao Chen <feihao_chen@trendmicro.com.cn>
2016-12-15 17:47:55 +0800

    Update salineup_enhancement

--- src/salineup_enhancement/behaviour_report_helper.py
+++ src/salineup_enhancement/behaviour_report_helper.py
@@ -1,56 +1,167 @@
  # Author: Feihao Chen
- # Date: 2016/12/13
+ # Date: 2016/12/15
 
 import os,sys
+import re
 import xml.etree.cElementTree as ET
 
-class BehaviourReport(object):
+class BehaviourReportHelper(object):
     """
 
     """
-    def __init__(self,xml_file):
-        self.root=self.root_reader(xml_file)
-        self.line_reader=self.line_reader(xml_file)
-        self.behaviour_reader=self.behaviour_reader()
-        self.filename=self.get_name()
-        self.sa_decison=self.get_decision()
-        self.rules=self.get_rules()
+    def __init__(self,xml_file = ''):
+        self.xml_file = xml_file
+        self.root = None
+        self.file_path = ''
+        self.sha1 = ''
+        self.decision = ''
+        self.rules = ''
+        self.behaviour = ''
+        self.feature_dict = {}
+        self.is_local = False
+        self.obfuscation = False
+        self.url_list = []
 
-    # read xml by root
-    def root_reader(self,xml_file):
-        tree=ET.parse(xml_file)
-        return tree.getroot()
+    def clear(self):
+        self.xml_file = ''
+        self.root = None
+        self.file_path = ''
+        self.sha1 = ''
+        self.decision = ''
+        self.rules = ''
+        self.behaviour = ''
+        self.feature_dict = {}
+        self.is_local = False
+        self.obfuscation = False
+        self.url_list = []
 
-    # read xml by line
-    def line_reader(self,xml_file):
-        with open(xml_file,'r') as file:
-            return file.read()
+    def set_xml_file(self, xml_file):
+        self.xml_file = xml_file
 
-    def behaviour_reader(self):
-        s=''
+    def parse_XML(self):
+        self.root = ET.parse(self.xml_file).getroot()
+        self.parse_base_info()
+        self.parse_internal()
+
+    def parse_base_info(self):
+        for e in self.root:
+            if e.tag == 'file_path':
+                self.file_path = e.text
+            elif e.tag == 'sha1':
+                self.sha1 = e.text
+            elif e.tag == 'decision':
+                self.decision = e.text
+            elif e.tag == 'matched_rules':
+                self.rules = ';'.join(x.text for x in e)
+
+    def parse_internal(self):
+        self.extract_behavior()
+        # judge whether it's a local script
+        if self.check_local_script_feature() and not self.check_browser_feature():
+            self.is_local = True
+        self.other_features()
+        # judge whether obfuscation exists
+        if 'window.eval' == self.feature_dict['\\bwindow\.[a-z0-9]+']:
+            self.obfuscation = True
+        # drag URLs out
+        self.count_url()
+
+    def extract_behavior(self):
         label=0
-        for e in self.root: 
-            if e.tag!='evidence':
-                continue
-            if e.attrib['type']=='javascript' and '// This is JS Runtime file' in e[2].text:
-                label=1
-                continue
-            if e.attrib['type']=='javascript_behavior' and label==1:
-                label=0
-                continue
-            s=s+e[-1].text
-        return s.lower()
+        for e in self.root:
+            if e.tag == 'evidence':
+                if e.attrib['type']=='javascript' and '// This is JS Runtime file' in e[2].text:
+                    label=1
+                    continue
+                if e.attrib['type']=='javascript_behaviour' and label==1:
+                    label=0
+                    continue
+                self.behaviour = self.behaviour + e[-1].text.lower()
 
-    def get_name(self):
-        return self.root[0].text
+    def add_feature_exists(self,feature):
+        self.feature_dict[feature] = 1 if re.search(feature,self.behaviour)!=None else 0
+
+    def add_feature_matched(self,feature):
+        self.feature_dict[feature] = ';'.join(set(re.findall(feature,self.behaviour)))
+
+    def check_local_script_feature(self):
+        self.add_feature_exists(r'/*@cc_on')
+        self.add_feature_exists('wscript.shell')
+        self.add_feature_exists('shell.application')
+        self.add_feature_exists('scripting.filesystemobject')
+        flag=0
+        flag+=self.feature_dict[r'/*@cc_on']
+        flag+=self.feature_dict['wscript.shell']
+        flag+=self.feature_dict['shell.application']
+        flag+=self.feature_dict['scripting.filesystemobject']
+        return True if flag>0 else False
+
+    def check_browser_feature(self):
+        self.add_feature_matched('\\bwindow\.[a-z0-9]+')
+        self.add_feature_matched('\\bdocument\.[a-z0-9]+')
+        self.add_feature_matched('\\bwindow\[[a-z0-9]+\]')
+        self.add_feature_matched('\\bdocument\[[a-z0-9]+\]')
+        self.add_feature_exists('getElementsByTagName')
+        self.add_feature_exists('getElementById')
+        self.add_feature_exists('<div')
+        self.add_feature_exists('\\bconsole\.')
+        self.add_feature_exists('parentNode')
+        self.add_feature_exists('\s\$\.')
+        flag=0
+        if self.feature_dict['\\bwindow\.[a-z0-9]+'] and self.feature_dict['\\bwindow\.[a-z0-9]+']!='window.eval':
+            flag+=1
+        if self.feature_dict['\\bdocument\.[a-z0-9]+']:
+            flag+=1
+        if self.feature_dict['\\bwindow\[[a-z0-9]+\]'] and self.feature_dict['\\bwindow\[[a-z0-9]+\]']!='window[eval]':
+            flag+=1
+        if self.feature_dict['\\bdocument\[[a-z0-9]+\]']:
+            flag+=1
+        flag+=self.feature_dict['getElementsByTagName']
+        flag+=self.feature_dict['getElementById']
+        flag+=self.feature_dict['<div']
+        flag+=self.feature_dict['\\bconsole\.']
+        flag+=self.feature_dict['parentNode']
+        flag+=self.feature_dict['\s\$\.']
+        return True if flag>0 else False
+
+    def other_features(self):
+        self.add_feature_exists('xmlhttp')
+        self.add_feature_exists('adodb.stream')
+
+    def count_url(self):
+        line_list=self.root[-1][-1].text.split('\n')
+        for line in line_list[2:]:
+            if line.find(' URL =')!=-1:
+                start=line.find(' URL =')+7
+                if line[start:start+4]=='http':
+                    start=start+7              #pass http://
+                    end=line.find('/',start)     
+                    if end ==-1:
+                        end=line.find('\"',start)
+                else:           #other url
+                    end=line.find('\"',start)
+                self.url_list.append(line[start:end])
+
+    def get_file_path(self):
+        return self.file_path
+
+    def get_sha1(self):
+        return self.sha1
 
     def get_decision(self):
-        for e in self.root:
-            if e.tag=='decision':
-                return e.text
+        return self.decision
 
     def get_rules(self):
-        for e in self.root:
-            if e.tag=='matched_rules':
-                return ';'.join(x.text for x in e)
+        return self.rules
 
+    def get_feature_dict(self):
+        return self.feature_dict
+
+    def get_is_local(self):
+        return self.is_local
+
+    def get_obfuscation(self):
+        return self.obfuscation
+
+    def get_url_list(self):
+        return self.url_list
--- src/salineup_enhancement/salineup_enhancement.py
+++ src/salineup_enhancement/salineup_enhancement.py
@@ -1,11 +1,11 @@
  # Author: Feihao Chen
- # Date: 2016/12/14
+ # Date: 2016/12/15
 
 import os,sys
 import csv
 import url_rank
 import xml.etree.cElementTree as ET
-from behaviour_report_helper import BehaviourReport
+from behaviour_report_helper import BehaviourReportHelper
 
 # analysis all .xml in sys.argv[1]
 # judge JS confusion and urls order
@@ -14,50 +14,35 @@ class MalDetHelper(object):
 
     """
     def __init__(self):
+        self.BRH = BehaviourReportHelper()
         csvfile=file('analysis_log.csv','wb')
         writer=csv.writer(csvfile)
         writer.writerow(['sample-path','obfuscation','URL','rank','SA_decision'])
         csvfile.close()
 
-    def obfuscation_exists(self,evidence):
-        content=evidence[-1].text.split('\n')
-        if content[1][:20].find('window.eval')!=-1:
-            return True
-        else:
-            return False
-
-    def url_judge(self,evidence):
-        line_list=evidence[-1].text.split('\n')
+    def url_judge(self,url_list):
         cur_no=0
         cur_url=''
-        for line in line_list[2:]:
-            if line.find(' URL =')!=-1:
-                start=line.find(' URL =')+7
-                if line[start:start+4]=='http':
-                    start=start+7              #pass http://
-                    end=line.find('/',start)     
-                    if end ==-1:
-                        end=line.find('\"',start)
-                else:           #other url
-                    end=line.find('\"',start)
-                url=line[start:end]
-                no=url_rank.analysis_url(url)
-                if no>cur_no:
-                    cur_no=no
-                    cur_url=url
+        for url in url_list:
+            no=url_rank.analysis_url(url)
+            if no>cur_no:
+                cur_no=no
+                cur_url=url
         if not cur_no:
             cur_no=''
         return [cur_url,cur_no]
 
     def report_append(self, xml_file):
-        BR = BehaviourReport(xml_file)
-        ob=self.obfuscation_exists(BR.root[-1])
-        cur_url,cur_no=self.url_judge(BR.root[-1])
+        self.BRH.set_xml_file(xml_file)
+        self.BRH.parse_XML()
+        cur_url,cur_no=self.url_judge(self.BRH.get_url_list())
 
         csvfile=file('analysis_log.csv','ab')
         writer=csv.writer(csvfile)
-        writer.writerow([BR.filename,str(ob),cur_url,str(cur_no),BR.sa_decison])
+        writer.writerow([self.BRH.get_file_path(),str(self.BRH.get_obfuscation()),cur_url,str(cur_no),self.BRH.get_decision()])
         csvfile.close()
+
+        self.BRH.clear()
 
 def print_usage():
     print """
